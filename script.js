4e0cfb4b1cb78a7d1f72b67ee4f28990a2e67176
ShreeramNew <shreerambca1@gmail.com>
2024-03-13 00:21:41 +0530

    fetching a local API

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ document.getElementById("Save").addEventListener("click", () => {
    let substring = "let";
    let initialIndex = text.indexOf(substring);
    let allIndexe = [];
-   let tempIndex=initialIndex;
-   let index=initialIndex;
+   let tempIndex = initialIndex;
+   let index = initialIndex;
    if (initialIndex !== -1) {
       while (tempIndex !== -1) {
          allIndexe.push(tempIndex);
-         tempIndex = text.indexOf(substring, tempIndex+ 1);
+         tempIndex = text.indexOf(substring, tempIndex + 1);
       }
       console.log(allIndexe);
       allIndexe.forEach(() => {
@@ -20,10 +20,28 @@ document.getElementById("Save").addEventListener("click", () => {
             "</span>" +
             text.substring(index + substring.length, text.length);
          console.log(newText);
-         index = newText.indexOf(substring, index + substring.length+28);
-         console.log('New Index:',index);
+         index = newText.indexOf(substring, index + substring.length + 28);
+         console.log("New Index:", index);
          input.innerHTML = newText;
          text = newText;
       });
    }
+});
+
+document.getElementById("Submit").addEventListener("click",async () => {
+   let email = document.getElementById("email");
+   let name = document.getElementById("name");
+   let data = {
+      name: "Shreeram",
+      email: "Shreerambca1@gmail.com",
+   };
+   let result =await fetch("http://localhost:3000/", {
+      method: "POST",
+      headers: {
+         "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+   })
+   let finalResult=await result.json();
+   console.log(finalResult);
 });
